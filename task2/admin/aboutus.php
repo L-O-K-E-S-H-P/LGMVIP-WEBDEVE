b5b0696f21b2e255112c84dcdb1c119481d16574
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2021-09-12 09:54:27 +0530

    Add files via upload

---
+++ task2/admin/aboutus.php
@@ -0,0 +1,32 @@
+  
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../css/aboutus.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">HOME</a></li>
+                <li><a href="aboutus.php">ABOUT</a></li>
+                <li><a href="contactus.php">CONTACT</a></li>
+                <li><a href="../login.php">ADMIN LOGIN</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Us</h2>
+             <div class="div2">
+             <h3>Student Result Management System</h2>
+             <p>Student Result Management System is a technological opportunity for the school, college, university and coaching centre institutions searching for a secure, simple and alternative solution to the conventional paper-based exam results evaluation, reporting and distribution.</p>
+             <p>A Student Management System In PHP and MySQL stores the grades per subject and grade level of each student per periodic grading. The student final grade for each subject will be automatically calculated and labeled the status if either the student passed or failed the subject. The system was minimally based on the process of storing and managing the student records in one of the secondary public schools.</p>
+             </div>  
+          </div>
+    </header>
+    
+</body>
+</html>
